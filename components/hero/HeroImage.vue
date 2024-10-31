e74a741b0578921d4b8d139f3c4c27986a3a13ff
Ahmosys <ahmosyspro@protonmail.com>
2024-10-31 09:17:40 +0100

    feat: initial commit

--- components/hero/HeroImage.vue
+++ components/hero/HeroImage.vue
@@ -1,6 +1,6 @@
 <template>
   <img
-    src="/hero-pfp.jpg"
+    src="/hero-pfps.jpg"
     alt="Profile Picture"
     class="rounded-full h-[13rem] w-[13rem] object-cover my-5 border-2 transition-transform ease-in-out duration-500 hover:scale-110"
   />
